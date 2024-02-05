db11aeb672801c058455fbd1f90464891150a851
Animesh Biswas <62756255+AnyMesss@users.noreply.github.com>
2024-02-05 04:00:22 +0000

    conversions Operations

--- 01_basics/03_conversionsOperation.js
+++ 01_basics/03_conversionsOperation.js
@@ -32,9 +32,31 @@ let value = 3
 let negValue = -value
 // console.log(negValue);
 
-console.log(2+2);
-console.log(2-2);
-console.log(2*2);
-console.log(2**2);
-console.log(2/3);
-console.log(2%3);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Animesh"
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("2"+2);//22
+// console.log(2+"2");//22
+// console.log("2"+2+2);//222
+// console.log(2+2+"2");//42
+
+console.log(+true);// 1
+// console.log(true+); // error
+console.log(+""); // 0
+
+let num1, num2, num3
+
+let gameCounter = 100
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
