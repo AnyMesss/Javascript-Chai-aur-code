26682695dd254f3f819a559168f5f41bc673c36f
Animesh Biswas <62756255+AnyMesss@users.noreply.github.com>
2024-02-05 03:25:20 +0000

    conversions of datatypes

---
+++ 01_basics/03_conversionsOperation.js
@@ -0,0 +1,26 @@
+// let score = "33abc" // NaN
+// let score = null // zero
+// let score = undefined // NaN
+// let score = true // false
+// let score = "Animesh" // NaN
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber); // NaN -> Not a Number
+// console.log(typeof(valueInNumber));
+
+
+// let isLoggedIn = 1 // true
+// let isLoggedIn = "" // false
+// let isLoggedIn = "Animesh" // true 
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33 // string
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
