f368c3a10ab15fdfeaf1e42e62990841d5598a8f
Animesh Biswas <62756255+AnyMesss@users.noreply.github.com>
2024-02-05 04:24:12 +0000

    comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,16 @@
+console.log("2">1);
+console.log("02">1);
+
+
+console.log(null>0);
+console.log(null==0);  
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>0);
+// Avoid such types of comparisions it will create confusions
+
+//equality and comparisions works differently
+
+console.log("2" === 2); // equality do the comparisions strictly as compared to normal ones
